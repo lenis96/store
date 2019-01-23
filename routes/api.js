b5740f2870dc5ab661cb82a0cd71483b0db86220
lenis96 <lenisandres5@gmail.com>
2019-01-23 14:29:39 -0500

    agregado jwt

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,9 @@
 const express=require('express')
 const productsRoutes=require('./productsRoutes')
+const auth=require('./auth')
 const api=express.Router()
-
 api.use('/product',productsRoutes)
+api.use('/auth',auth)
 
 
 
